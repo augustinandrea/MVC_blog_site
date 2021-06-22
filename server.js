aebc79fe05539373d49c7531016e11935326ce9b
augustinandrea <augustin.andrea@gmail.com>
2021-06-21 18:13:37 -0700

    Added the various folders and config, server, and db work.

---
+++ server.js
@@ -0,0 +1,7 @@
+// Dependencies
+const path = require("path");
+const express = require("express");
+
+// all routes asdefined in the controllers folder
+const routes = require("./controllers/");
+const sequelize = require("./config/connection");
