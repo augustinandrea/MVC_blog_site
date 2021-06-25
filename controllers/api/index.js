31039530e33f99e8559018219439c6ccffff682b
augustinandrea <augustin.andrea@gmail.com>
2021-06-24 20:08:18 -0700

    Javascript files for how the website would work.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,4 @@
-  const router = require('express').Router();
+const router = require('express').Router();
 
 const userRoutes = require('./user_routes');
 const postRoutes = require('./post_routes');
